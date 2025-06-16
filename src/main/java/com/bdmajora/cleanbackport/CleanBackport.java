c5a9de3a82a0603348917c052cc98250bdc13b20
bdmajora <bdmajora@protonmail.com>
2025-06-16 14:18:06 -0600

    The First Commit of A CleanBackport

--- src/main/java/com/bdmajora/cleanbackport/CleanBackport.java
+++ src/main/java/com/bdmajora/cleanbackport/CleanBackport.java
@@ -1,13 +1,12 @@
-package com.example.modid;
+package com.bdmajora.cleanbackport;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class CleanBackport {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
