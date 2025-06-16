92560dc6102c5ed9dab33e08304fd86ed9864e85
bdmajora <bdmajora@protonmail.com>
2025-06-16 15:14:51 -0600

    Added the Client code for the Crossbow

---
+++ src/main/java/com/bdmajora/cleanbackport/client/ClientProxy.java
@@ -0,0 +1,39 @@
+package com.bdmajora.cleanbackport.client;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import com.bdmajora.cleanbackport.common.CommonProxy;
+import com.bdmajora.cleanbackport.common.Constants;
+import com.bdmajora.cleanbackport.common.CrossbowsContent;
+
+@EventBusSubscriber(value = Side.CLIENT, modid = Constants.MODID)
+public class ClientProxy extends CommonProxy {
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
+
+    @SubscribeEvent
+    public static void registerModels(ModelRegistryEvent event) {
+        ModelLoader.setCustomModelResourceLocation(CrossbowsContent.CROSSBOW, 0, new ModelResourceLocation(Constants.loc("Crossbow"), "normal"));
+    }
+
+}
